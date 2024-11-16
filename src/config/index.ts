ef1bb7bd2e56e66be400c26666027d5a97138973
Aridan Pantoja <aridanpantoja@gmail.com>
2024-11-16 00:13:40 -0300

    chore: add siteconfig file

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: 'Babel Fish',
+  description: 'The easiest way to understand your fish.',
+  url: '',
+  ogImage: '',
+  links: {
+    github: 'https://github.com/aridanpantoja/babel-fish',
+  },
+}
