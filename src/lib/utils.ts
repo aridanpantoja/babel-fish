5822d6b22a74cd9e8ce4da47d81a72120dbb9213
Aridan Pantoja <aridanpantoja@gmail.com>
2024-11-16 02:27:51 -0300

    feat: add localStorage and date formatting utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
 import { clsx, type ClassValue } from 'clsx'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getLocalStorage(key: string) {
+  if (typeof window === 'undefined') return
+
+  const value = localStorage.getItem(key)
+
+  if (!value) return
+
+  return JSON.parse(value)
+}
+
+export function setLocalStorage(key: string, value: string) {
+  if (typeof window === 'undefined') return
+
+  localStorage.setItem(key, value)
+}
+
+export function formatDate(value: Date) {
+  return format(value, 'PPP', { locale: ptBR })
 }
