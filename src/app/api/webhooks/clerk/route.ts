4ddb7d7403c9011cb3b2a6635d135f088f4d4971
Aridan <aridanpantoja@gmail.com>
2024-11-21 15:19:51 -0300

    feat: add clerk webhook and make it public

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,77 @@
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET
+
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      'Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local',
+    )
+  }
+
+  const wh = new Webhook(SIGNING_SECRET)
+
+  const headerPayload = await headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error: Missing Svix headers', {
+      status: 400,
+    })
+  }
+
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
+
+  let evt: WebhookEvent
+
+  try {
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error: Could not verify webhook:', err)
+    return new Response('Error: Verification error', {
+      status: 400,
+    })
+  }
+
+  const eventType = evt.type
+
+  if (eventType === 'user.created') {
+    const { id, email_addresses, first_name, last_name, image_url } = evt.data
+
+    if (!id || !email_addresses) {
+      return new Response('Error occurred -- missing data', {
+        status: 400,
+      })
+    }
+
+    const user = {
+      clerkUserId: id,
+      email: email_addresses[0].email_address,
+      ...(first_name ? { firstName: first_name } : {}),
+      ...(last_name ? { lastName: last_name } : {}),
+      ...(image_url ? { imageUrl: image_url } : {}),
+    }
+    // TODO: await createUser(user as User)
+
+    return new Response(`Usuário criado com sucesso: ${user}`, {
+      status: 200,
+    })
+  }
+
+  if (eventType === 'user.deleted') {
+    return new Response('Usuário deletado com sucesso', { status: 200 })
+  }
+
+  if (eventType === 'user.updated') {
+    return new Response('Usuário atualizado com sucesso', { status: 200 })
+  }
+}
