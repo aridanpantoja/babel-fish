e371db619274f1344d86a91468821606003a8169
Aridan Pantoja <aridanpantoja@gmail.com>
2024-11-20 23:33:24 -0300

    feat: add Portuguese (Brazil) localization to ClerkProvider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,10 @@
 import { FishProvider } from '@/providers/fish-context'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ptBR } from '@clerk/localizations'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <FishProvider>{children}</FishProvider>
     </ClerkProvider>
   )
